b7dba5943c34820dc2cf52bc67ab4de556062f9c
MrLoop <46170910+Mr-L00P@users.noreply.github.com>
2026-03-06 19:59:33 +0100

    Update pam_cbac_func.h

--- PAM/include/pam_cbac_func.h
+++ PAM/include/pam_cbac_func.h
@@ -1,3 +1,11 @@
+
+// pam_cbac_func.h 
+// Definitions and declarations for CBAC
+
+// TODO: Definir el resto de códigos de mensaje, mensajes enviados por PAM
+//       IMPORTANTE: También definir en cbacd.py
+
+
 #ifndef PAM_CBAC_AUX_H
 #define PAM_CBAC_AUX_H
 
@@ -16,11 +24,18 @@ struct pam_cbac_packet_t {
     char message[PAM_CBAC_MSG_SIZE];
 };
 
+
 // Message codes
-#define CBAC_SUCCESS     0  // User exists and has a reservation
-#define CBAC_WRONG_USER  1  // No reservation, occupied space
-#define CBAC_EMPTY_SPACE 2  // No reservation but empty space
-#define CBAC_API_ERROR   3  // Daemon couldn't process request with Google API
+#define CBAC_SUCCESS       0  // User exists and has a reservation
+#define CBAC_WRONG_USER    1  // No reservation, occupied space
+#define CBAC_EMPTY_SPACE   2  // No reservation but empty space
+#define CBAC_DAEMON_ERROR  3  // Daemon couldn't process request with Google API
+
+#define CBAC_CHECK_RESERV  10 // Asks daemon to check if user can go through. Message set to username to check
+#define CBAC_MAKE_RESERV   11 // Asks daemon to make a reservation from now   Message set to time interval desired
+#define CBAC_ADD_USER      12 // Asks daemon to add user to the calendar of the system
+
+
 
 // Info conv macros
 #define CBAC_OKAY(pamh, msg, ...) pam_info(pamh, "[+] - " msg "\n", ##__VA_ARGS__)
